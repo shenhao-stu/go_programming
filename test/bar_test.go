c6a0baf05f642b90205cd0aa41c1798a7cf80d20
yingpei <56550322@163.com>
2022-08-08 18:01:24 +0800

    codes(testing): learn how to use testing

--- test/bar_test.go
+++ test/bar_test.go
@@ -1,18 +1,22 @@
-package main
+package test
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 var p *int
 
 // 作用域，函数只能收到全局变量，另一个函数的局部变量不会影响bar()
+// 函数的声明在外部
 func bar() {
 	//use p
 	fmt.Println(*p)
 }
 
-func main() {
+func TestBar(t *testing.T) {
 	a := 1
 	p := &a
 	bar()
-	fmt.Println(*p)
+	t.Log(*p)
 }
